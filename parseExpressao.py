37af2569c6b715dbdd487eb0f65a71e1c3371d16
Leonardo Saito <leosaito632@gmail.com>
2026-03-14 15:23:59 -0300

    base das funcoes para parseExpressao

---
+++ parseExpressao.py
@@ -0,0 +1,19 @@
+def parseExpressao(linha: str) -> list[str]:
+    print("Olá Mundo!")
+    return [""]
+
+
+def estadoNumero():
+    pass
+
+
+def estadoOperador():
+    pass
+
+
+def estadoParenteses():
+    pass
+
+
+def estadoComandos():
+    pass
