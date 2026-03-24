5e1d0d17c5238554bb99f19d89ba1751c1689e76
Leonardo Saito <leosaito632@gmail.com>
2026-03-24 01:46:33 -0300

    parseExpressao refatorado para AFD, testes necessarios

--- parseExpressao.py
+++ parseExpressao.py
@@ -55,17 +55,24 @@ class AnalisadorLexico:
     linha_atual: int  # Linha do arquivo que está sendo analisada
     coluna_atual: int  # Posição (coluna) da linha que está sendo analisada
     expressao: str  # Conteúdo completo da linha que está sendo analisada
+    tokens_linha_atual: list[Token] = []  # Tokens da expressao atual
     # Matriz para armazenar todos os tokens de todas as expressoes (linhas)
     matriz_tokens: list[list[Token]]
 
     def __init__(self):
         self.linha_atual = 0
         self.coluna_atual = 0
+        self.tokens_linha_atual = []
         self.matriz_tokens = []
 
     def is_fim_token(self) -> bool:
-        # O fim de um token é o espaço " " ou um parêntese esquerdo ")"
-        return self.peek_atual() == SEPARADOR_TOKEN or self.peek_atual == ")"
+        # O fim de um token é o espaço " ", um parêntese esquerdo ")" ou string vazia ""
+
+        return (
+            self.peek_atual() == SEPARADOR_TOKEN
+            or self.peek_atual() == ")"
+            or self.peek_atual() == ""
+        )
 
     def avancar(self):
         """Avanca um char e atualiza as posições"""
@@ -82,6 +89,11 @@ class AnalisadorLexico:
     def parseExpressao(self, expressao: str, numero_linha: int):
         """Função chamada para cada linha (expressão) que será analisada"""
 
+        # Caso não seja a primeira linha
+        # if self.tokens_linha_atual:
+        #    self.matriz_tokens.append(self.tokens_linha_atual)
+        #    self.tokens_linha_atual = []
+
         # Prepara o estado para a nova linha
         self.expressao = expressao
         self.linha_atual = numero_linha
@@ -93,22 +105,34 @@ class AnalisadorLexico:
         atual = self.peek_atual()
         token = Token(coluna=self.coluna_atual)
 
-        if atual in NUMEROS_VALIDOS:
+        # Chegou no fim da expressao
+        if not atual:
+            self.matriz_tokens.append(self.tokens_linha_atual)
+            self.tokens_linha_atual = []
+
+        elif atual == SEPARADOR_TOKEN:
+            self.avancar()
+            self.estadoInicial()
+
+        elif atual in NUMEROS_VALIDOS:
             self.estadoNumero(token=token)
 
         elif atual in OPERADORES_VALIDOS:
             self.estadoOperador(token)
 
-        elif atual in PARENTESES:
-            self.estadoParentese(self.coluna_atual)
+        elif atual == "(":
+            self.estadoParenteseDir(token)
+
+        elif atual == ")":
+            self.estadoParenteseEsq(token)
 
         # Keyword RES
         elif atual == "R":
-            self.estadoComandoRes(self.coluna_atual)
+            self.estadoComandoR(token)
 
         # Identificadores de Memória são compostos de letras maíusculas
         elif atual.isalpha() and atual.isupper():
-            self.estadoComandoMemoria(col_inicio=self.coluna_atual, lexema=None)
+            self.estadoComandoMemoria(token)
         else:
             self.estadoErro(token)
 
@@ -123,9 +147,27 @@ class AnalisadorLexico:
         if self.peek_atual() in NUMEROS_VALIDOS:
             self.estadoNumero(token)
         elif self.peek_atual() == SEPARADOR_DECIMAL:
-            self.estadoDecimal(token)
+            self.estadoPonto(token)
         elif self.is_fim_token():
             self.estadoFimToken(token)
+        else:
+            self.estadoErro(token)
+
+    def estadoPonto(self, token: Token):
+        """Estado para '.' logo depois da parte inteira do número.
+        Não é um estado final. Deve ter mais números depois do '.' para ser válido"""
+
+        # Muda o tipo do Token para NUMERO_REAL
+        token.tipo = TipoToken.NUMERO_REAL
+        # Adiciona o valor da posição atual ao valor do token
+        token.valor += self.peek_atual()
+
+        self.avancar()
+
+        if self.peek_atual() in NUMEROS_VALIDOS:
+            self.estadoDecimal(token)
+        else:
+            self.estadoErro(token)
 
     def estadoDecimal(self, token: Token):
         """Estado para números com casas decimais.
@@ -168,81 +210,87 @@ class AnalisadorLexico:
             self.estadoErro(token)
 
     def estadoParenteseDir(self, token: Token):
-        self.tipo = TipoToken.PARENTESE_DIR
-        self.valor = self.peek_atual()
+        token.tipo = TipoToken.PARENTESE_DIR
+        token.valor = self.peek_atual()
         self.avancar()
 
-        if self.peek_atual() == SEPARADOR_TOKEN:
-            self.estadoFimToken(token)
-        else:
-            self.estadoErro(token)
+        # Qualquer coisa pode vir depois de um PARENTESE_DIR
+        self.estadoFimToken(token)
 
     def estadoParenteseEsq(self, token: Token):
-        self.tipo = TipoToken.PARENTESE_ESQ
-        self.valor = self.peek_atual()
+        token.tipo = TipoToken.PARENTESE_ESQ
+        token.valor = self.peek_atual()
         self.avancar()
 
-        if self.peek_atual() == SEPARADOR_TOKEN:
+        if self.is_fim_token():
             self.estadoFimToken(token)
         else:
             self.estadoErro(token)
 
     def estadoComandoR(self, token: Token):
-        self.tipo = TipoToken.KEYWORD
-        self.valor = self.peek_atual()
+        token.tipo = TipoToken.KEYWORD
+        token.valor += self.peek_atual()
+        self.avancar()
 
         if self.peek_atual() == "E":
             self.estadoComandoE(token)
+        elif self.peek_atual().isalpha() and self.peek_atual().isupper():
+            self.estadoComandoMemoria(token)
+        else:
+            self.estadoErro(token)
 
     def estadoComandoE(self, token: Token):
-        self.tipo = TipoToken.KEYWORD
-        self.valor = self.peek_atual()
+        token.tipo = TipoToken.KEYWORD
+        token.valor += self.peek_atual()
+        self.avancar()
 
         if self.peek_atual() == "S":
-            self.estadoComandoE(token)
+            self.estadoComandoS(token)
+        elif self.peek_atual().isalpha() and self.peek_atual().isupper():
+            self.estadoComandoMemoria(token)
+        else:
+            self.estadoErro(token)
 
     def estadoComandoS(self, token: Token):
-        self.tipo = TipoToken.KEYWORD
-        self.valor = self.peek_atual()
+        token.tipo = TipoToken.KEYWORD
+        token.valor += self.peek_atual()
+        self.avancar()
 
         if self.is_fim_token():
             self.estadoFimToken(token)
+        elif self.peek_atual().isalpha() and self.peek_atual().isupper():
+            self.estadoComandoMemoria(token)
+        else:
+            self.estadoErro(token)
 
-    def estadoComandoMemoria(self, col_inicio: int, lexema: str | None) -> Token:
-        # Se lexema não foi inicializado ainda, inicializa com o atual
-        if not lexema:
-            lexema = self.peek_atual()
-            self.avancar()
+    def estadoComandoMemoria(self, token: Token):
+        token.tipo = TipoToken.MEMORIA
+        token.valor += self.peek_atual()
+        self.avancar()
 
-        while self.peek_atual().isalpha():
-            if not self.peek_atual().isupper():
-                raise ErroTokenInvalido(
-                    "Caractere minúsculo encontrado no Identificador de Memória",
-                    self.linha_atual,
-                    self.coluna_atual,
-                )
-            lexema += self.peek_atual()
-            self.avancar()
-
-        return Token(
-            tipo=TipoToken.MEMORIA,
-            valor=lexema,
-            linha=self.linha_atual,
-            coluna=col_inicio,
-        )
+        if self.peek_atual().isalpha() and self.peek_atual().isupper():
+            self.estadoComandoMemoria(token)
+        elif self.is_fim_token():
+            self.estadoFimToken(token)
+        else:
+            self.estadoErro(token)
 
     def estadoFimToken(self, token: Token):
-        self.matriz_tokens.append([token])
+        self.tokens_linha_atual.append(token)
+        self.estadoInicial()
 
     def estadoErro(self, token: Token):
-        raise ErroTokenInvalido("", token.linha, token.coluna)
+        print(f"Estado de erro! {token}")
+        raise ErroTokenInvalido(f"{token.valor}", token.linha, token.coluna)
 
 
 if __name__ == "__main__":
     analisador = AnalisadorLexico()
 
-    analisador.parseExpressao("1.0 3 - 5 9 * (1.5 X) RES 1.1 () RESRES", 1)
-    analisador.parseExpressao("1.0 3 - 5 9 * (1.5 X) RES 1.1 () RESRES", 2)
+    analisador.parseExpressao(
+        "1 11 1.1 // / + - % ^ RES MEM TESTE GAMER () ( ) (11 1 +) (1 MEM)", 1
+    )
+
     for i in analisador.matriz_tokens:
         for j in i:
             print(j)
