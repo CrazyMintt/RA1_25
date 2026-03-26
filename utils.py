0f7bb6ebcd6532e7914d0cb1e0e14ff7d60d11ff
Brunobetiatto <betiattobruno@gmail.com>
2026-03-26 19:36:25 -0300

    retirando salvar_tokens, salvar_assembly e argumentos em linha de comando da main e passando para utils

---
+++ utils.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+def salvar_tokens(nome_base, matriz_tokens):
+    nome_saida = f"{nome_base}_tokens.txt"
+    try:
+        with open(nome_saida, 'w', encoding='utf-8') as f:
+            for i, linha_tokens in enumerate(matriz_tokens, start=1):
+                f.write(f"Linha {i}: ")
+                # Converte cada objeto Token em string para salvar
+                tokens_str = [f"[{t.tipo.name}: {t.valor}]" for t in linha_tokens]
+                f.write(" ".join(tokens_str) + "\n")
+        print(f"Tokens salvos com sucesso em: {nome_saida}")
+    except Exception as e:
+        print(f"Erro ao salvar tokens: {e}")
+
+
+def salvar_assembly(nome_base, codigo_assembly):
+    nome_saida = f"{nome_base}.s"
+    try:
+        with open(nome_saida, 'w', encoding='utf-8') as f:
+            f.write(codigo_assembly)
+        print(f"Código Assembly salvo com sucesso em: {nome_saida}")
+    except Exception as e:
+        print(f"Erro ao salvar assembly: {e}")
+
+
+def obter_argumentos_cli():
+    if len(sys.argv) < 2:
+        print("Uso: python main.py arquivo.txt")
+        sys.exit(1)
+
+    caminho_arquivo = sys.argv[1]
+    
+    if not caminho_arquivo.lower().endswith(".txt"):
+        print("Erro: o arquivo deve ser .txt")
+        sys.exit(1)
+    nome_base = os.path.splitext(os.path.basename(caminho_arquivo))[0]
+    
+    return caminho_arquivo, nome_base
