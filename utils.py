ddd7a62c4d1947b06453919a1d9ea9fc6fa3f4a3
Leonardo Saito <leosaito632@gmail.com>
2026-03-26 23:29:31 -0300

    parenteses nos testes + nomes nos arquivos

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+'''
+Equipe:
+Bruno Betiatto Alves @Brunobetiatto
+Bruno Himovski Opuszka Machado Dutra @CrazyMintt 
+Leonardo Saito @Leosaito632 
+Vitor Nicoletti @vitorNicoletti
+
+GRUPO: RA1-25   
+'''
+
 import sys
 import os
 
